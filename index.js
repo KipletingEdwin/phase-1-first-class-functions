ae5b2bd284664dc450a73e23a99f5d60ad06b773
Kipleting <kipletingedwin4@gmail.com>
2022-06-08 07:41:07 +0300

    commit tree 17c3b991f412d768ca43640f47f036aad7986dc9

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function receivesAFunction(callback){
+    return callback()
+}
+receivesAFunction(() => 'new order')
+
+function returnsANamedFunction(){
+    return function neverQuit(){
+        console.log("Quitting is not an option")
+    }
+ 
+}
+function returnsAnAnonymousFunction(){
+    return function (){
+        return "new label"
+    }
+
+}
